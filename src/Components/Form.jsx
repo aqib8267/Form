c5d8f2ab58146f43b7811e5a434cd9b91dea7a40
MOHD AQIB <mohdaqib921@gmail.com>
2023-02-17 17:53:21 +0530

    user form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -17,7 +17,7 @@ const Form =()=>{
                 <span className='Title'>Enter the Details</span>
                 <input type="text" className='input-field' value={url} onChange={(e)=>setUrl(e.target.value)} name="url" placeholder='Enter Url' />
                 <input type="text" className='input-field' value={pages} onChange={(e)=>setPages(e.target.value)} name="pages" placeholder='Enter number of pages' />
-                <input type="text" className='input-field' value={info} onChange={(e)=>setInfo(e.target.value)} name="info" placeholder='Enter info' />
+                <input type="text" className='input-field' value={info} onChange={(e)=>setInfo(e.target.value)} name="info" placeholder='Report type' />
                 <button className='button btn-form'>Download</button>
             </form>
         </div>
